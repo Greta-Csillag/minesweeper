9c8240b6787ceb31393720a6997e20e6622b1163
greta <greta@DESKTOP-6N2E64G>
2021-09-22 19:09:51 +0200

    Add project files.

---
+++ minesweeper/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace minesweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(293, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Beállítások";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(40, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pályahossz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(550, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Aknák száma";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button1.Location = new System.Drawing.Point(344, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 27);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(43, 168);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(553, 167);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            899,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.ForeColor = System.Drawing.Color.DarkRed;
+            this.label6.Location = new System.Drawing.Point(262, 337);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(294, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Az aknák száma nagyobb a mezők számánál!";
+            this.label6.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ minesweeper/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace minesweeper
+{
+    
+    struct Cella
+    {
+        public int Line;
+        public int Row;
+        public Cella(int line, int row)
+        {
+            Line = line;
+            Row = row;
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public int hossz;
+        public int akna;
+        public PictureBox tiz = new PictureBox();
+        public PictureBox zaszlo = new PictureBox();
+        public PictureBox bomba = new PictureBox();
+        public bool vesztett = false;
+        public bool nyertes = false;
+        public int mezok = 0;
+        public int min = 0;
+        public int sec = 0;
+        public int[,] aknamező;
+        public PictureBox[,] kepek;
+        public int zaszlo_szam;
+        public PictureBox button2;
+        public Label label4;
+        public Label label5;
+        public Form1()
+        {
+            InitializeComponent();
+     
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hossz = Convert.ToInt32(numericUpDown1.Value);
+            akna = Convert.ToInt32(numericUpDown2.Value);
+            if (akna < hossz * hossz)
+            {
+                alap();
+                timer1.Start();
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                numericUpDown1.Visible = false;
+                numericUpDown2.Visible = false;
+                button1.Visible = false;
+                label6.Visible = false;
+                
+            }
+            else
+            {
+                label6.Visible = true;
+            }
+            
+        }
+
+
+        private void alap()
+        {
+            aknamező = new int[hossz, hossz];
+            kepek = new PictureBox[hossz, hossz];
+            zaszlo_szam = akna;
+
+            //felső sor létrehozása
+            button2 = new PictureBox();
+            button2.SizeMode = PictureBoxSizeMode.StretchImage;
+            button2.BackgroundImageLayout = ImageLayout.Stretch;
+            Bitmap emoji1 = new Bitmap("emoji_1.png");
+            button2.BackgroundImage = (Image)emoji1;
+            button2.Left = 300;
+            button2.Top = 20;
+            button2.Width = 25;
+            button2.Height = 25;
+            button2.Click += button2_Click;
+            this.Controls.Add(button2);
+
+            PictureBox picturebox1 = new PictureBox();
+            picturebox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            picturebox1.BackgroundImageLayout = ImageLayout.Stretch;
+            Bitmap bitmap4 = new Bitmap("12.png");
+            zaszlo.Image = (Image)bitmap4;
+            picturebox1.BackgroundImage =zaszlo.Image;
+            picturebox1.Left = 50;
+            picturebox1.Top = 20;
+            picturebox1.Width = 25;
+            picturebox1.Height = 25;
+            this.Controls.Add(picturebox1);
+
+            label4 = new Label();
+            label4.Text = ($"{akna}");
+            label4.Left = 80;
+            label4.Top = 20;
+            label4.Width = 25;
+            label4.Height = 25;
+            this.Controls.Add(label4);
+
+            PictureBox picturebox2 = new PictureBox();
+            picturebox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            picturebox2.BackgroundImageLayout = ImageLayout.Stretch;
+            Bitmap bitmap5 = new Bitmap("11.png");
+            picturebox2.BackgroundImage = (Image)bitmap5;
+            picturebox2.Left = 450;
+            picturebox2.Top = 20;
+            picturebox2.Width = 25;
+            picturebox2.Height = 25;
+            this.Controls.Add(picturebox2);
+
+
+            label5 = new Label();
+            label5.Text = "";
+            label5.Left = 475;
+            label5.Top = 20;
+            label5.Width = 50;
+            label5.Height = 25;
+            this.Controls.Add(label5);
+
+
+
+
+            int sor;
+            int oszlop;
+            Random r = new Random();
+            for (int i = 0; i < akna; i++)
+            {
+                sor = r.Next(0, hossz);
+                oszlop = r.Next(0, hossz);
+                while (aknamező[sor, oszlop] == -1)
+                {
+                    sor = r.Next(0, hossz);
+                    oszlop = r.Next(0, hossz);
+                }
+                aknamező[sor, oszlop] = -1;
+            }
+            for (int i = 0; i < hossz; i++)
+            {
+                for (int j = 0; j < hossz; j++)
+                {
+                    if (aknamező[i, j] == -1)
+                    {
+                        for (int k = -1; k < 2; k++)
+                        {
+                            for (int l = -1; l < 2; l++)
+                            {
+                                if (i - k >= 0 && i - k <= hossz - 1 && j - l >= 0 && j - l <= hossz - 1)
+                                {
+                                    if (aknamező[i - k, j - l] != -1)
+                                    {
+                                        aknamező[i - k, j - l]++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            kepbeolvaso();
+        }
+        private void kepbeolvaso()
+        {
+
+            Bitmap bitmap2 = new Bitmap("10.png");
+            tiz.Image = (Image)bitmap2;
+            Bitmap akna = new Bitmap("0.png");
+            bomba.Image = (Image)akna;
+            for (int i = 0; i < hossz; i++)
+            {
+                for (int j = 0; j < hossz; j++)
+                {
+
+                    PictureBox picture = new PictureBox();
+                    picture.SizeMode = PictureBoxSizeMode.StretchImage;
+                    if (hossz > 15)
+                    {
+                        picture.Left = 50 + j * 25;
+                        picture.Top = 50 + i * 25;
+                        picture.Width = 25;
+                        picture.Height = 25;
+                    }
+                    else
+                    {
+                        picture.Left = 50 + j * 50;
+                        picture.Top = 50 + i * 50;
+                        picture.Width = 50;
+                        picture.Height = 50;
+                    }
+                    Cella cella = new Cella(i, j);
+                    picture.Tag = cella;
+                    kepek[i, j] = picture;
+
+
+                    picture.Image = (Image)bitmap2;
+
+
+                    this.Controls.Add(picture);
+                    picture.Click += kep_klikk;
+                }
+            }
+
+        }
+        public void kep_klikk(object sender, System.EventArgs e)
+        {
+            if (vesztett == false && nyertes == false)
+            {
+                PictureBox clicked_pd = (PictureBox)sender;
+                Cella c = (Cella)(clicked_pd.Tag);
+                MouseEventArgs me = (MouseEventArgs)e;
+                if (me.Button == MouseButtons.Right)
+                {
+                    Zaszlo(c.Line, c.Row);
+                }
+                if (me.Button == MouseButtons.Left)
+                {
+                    Felfedes(c.Line, c.Row);
+                }
+            }
+        }
+        public void Felfedes(int sor, int oszlop)
+        {
+            if (kepek[sor, oszlop].Image == tiz.Image)
+            {
+                int a = aknamező[sor, oszlop];
+                if (a == -1)
+                {
+                    for (int i = 0; i < hossz; i++)
+                    {
+                        for (int j = 0; j < hossz; j++)
+                        {
+                            if (aknamező[i, j] == -1)
+                            {
+                                kepek[i, j].Image = bomba.Image;
+                                vesztett = true;
+                                Bitmap emoji3 = new Bitmap("emoji_3.png");
+                                button2.BackgroundImage = (Image)emoji3;
+                                timer1.Stop();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Bitmap bitmap1 = new Bitmap($"{a + 1}.png");
+                    kepek[sor, oszlop].Image = (Image)bitmap1;
+                    mezok++;
+                    if (mezok ==(hossz*hossz)-akna)
+                    {
+                        nyertes = true;
+                        Bitmap emoji2 = new Bitmap("emoji_2.png");
+                        button2.BackgroundImage = (Image)emoji2;
+                        timer1.Stop();
+                    }
+                }
+                if (aknamező[sor, oszlop] == 0)
+                {
+                    for (int k = -1; k < 2; k++)
+                    {
+                        for (int l = -1; l < 2; l++)
+                        {
+                            if (sor - k >= 0 && sor - k <= hossz - 1 && oszlop - l >= 0 && oszlop - l <= hossz - 1)
+                            {
+                                if (aknamező[sor - k, oszlop - l] != -1)
+                                {
+                                    Felfedes(sor - k, oszlop - l);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public void Zaszlo(int sor, int oszlop)
+        {
+            if (kepek[sor, oszlop].Image == tiz.Image)
+            {
+                kepek[sor, oszlop].Image = zaszlo.Image;
+                zaszlo_szam--;
+                label4.Text = ($"{zaszlo_szam}");
+            }
+            else if (kepek[sor, oszlop].Image == zaszlo.Image)
+            {
+                kepek[sor, oszlop].Image = tiz.Image;
+                zaszlo_szam++;
+                label4.Text = ($"{zaszlo_szam}");
+            }
+        }
+
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            min = 0;
+            sec = 0;
+            timer1.Start();
+            nyertes = false;
+            vesztett = false;
+            mezok = 0;
+            alap();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sec++;
+            if (sec == 60)
+            {
+                min++;
+                sec = 0;
+            }
+            label5.Text = ($"{min}:{sec}");
+        }
+        
+    }
+}
+
+
+// az első csak üres lehet
